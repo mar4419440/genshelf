437307daf519a0af00c163068ce6988e60dd95c6
mar <247403898+mahmoudelzakaiky-code@users.noreply.github.com>
2026-04-19 15:23:03 +0200

    Add missing storage relationship to Transaction model and finalize schema fixes

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -27,4 +27,9 @@ class Transaction extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function storage()
+    {
+        return $this->belongsTo(Storage::class);
+    }
 }
