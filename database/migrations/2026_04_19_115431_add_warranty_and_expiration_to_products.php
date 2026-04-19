1ae073cb0d28e41124f1dbbfa89b878a3132b43e
mar <247403898+mahmoudelzakaiky-code@users.noreply.github.com>
2026-04-19 13:56:29 +0200

    Add products warranty and expiration columns migration

---
+++ database/migrations/2026_04_19_115431_add_warranty_and_expiration_to_products.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->boolean('has_warranty')->default(false);
+            $table->boolean('has_expiration')->default(false);
+            $table->date('expiration_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn(['has_warranty', 'has_expiration', 'expiration_date']);
+        });
+    }
+};
